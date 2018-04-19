ba2b3ad35ab63e76d8f84aa9a88f6169526c4a03
fuyuanming <fymhunan@163.com>
2018-04-19 16:11:18 +0800

    第一次提交好紧张

---
+++ src/main/java/com/nexwise/controller/BaseController.java
@@ -0,0 +1,17 @@
+package com.nexwise.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BaseController {
+
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    @ResponseBody
+    public String sayHi() {
+        return "hello";
+    }
+
+}
