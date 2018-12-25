2c00cbc0c6b18e0e1fe39c4db192a41afa7e5c8c
xiaoming <fymhunan@163.com>
2018-12-25 17:49:11 +0800

    更新zookeeper案例

--- src/test/java/practice/Test.java
+++ src/test/java/practice/Test.java
@@ -2,6 +2,14 @@ package practice;
 
 public class Test {
 
+    public static void main(String[] args) {
+        String host = "http://45.32.217.231:2181";
+        int index = host.indexOf(47);
+        if (index > 0) {
+            System.out.println(host.substring(0, index));
+        }
+
+    }
 
 
 }
--- src/test/java/zookeeper/ZookeeperConnect.java
+++ src/test/java/zookeeper/ZookeeperConnect.java
@@ -15,8 +15,9 @@ import static org.apache.zookeeper.CreateMode.EPHEMERAL;
  * zookeeper连接工具类
  */
 public class ZookeeperConnect {
-    final CountDownLatch countDownLatch = new CountDownLatch(1);
-    String host = "127.0.0.1:2181";
+    //String host = "47.106.88.138:2181";
+    //String host = "45.32.217.231:2181";
+    String host = "45.32.217.231:2181,54.168.84.250:2181,47.106.88.138:2181";
     int sessionTimeout = 60000;
     ZooKeeper zooKeeper = null;
 
@@ -26,16 +27,13 @@ public class ZookeeperConnect {
      * @throws IOException
      * @throws InterruptedException
      */
-    public ZooKeeper connect() throws IOException, InterruptedException {
+    public ZooKeeper connect() throws IOException {
         zooKeeper = new ZooKeeper(host, sessionTimeout, new Watcher() {
             @Override
             public void process(WatchedEvent watchedEvent) {
-                if (watchedEvent.getState() == Event.KeeperState.SyncConnected) {
-                    countDownLatch.countDown();
-                }
+                System.out.println("触发监控事件类型：" + watchedEvent.getType());
             }
         });
-        countDownLatch.await();
         return zooKeeper;
     }
 
@@ -51,18 +49,17 @@ public class ZookeeperConnect {
         ZookeeperConnect connect = new ZookeeperConnect();
 
         ZooKeeper zooKeeper = connect.connect();
-        byte[] bytes = zooKeeper.getData("/FirstZnode", false, new Stat());
-        System.out.println(new String(bytes));
-        String path = "/FirstZnode";
-        byte[] data = "first znode data".getBytes("utf8");
-        List<ACL> acl = zooKeeper.getACL("/", new Stat());
+//        byte[] bytes = zooKeeper.getData("/FirstZnode", false, new Stat());
+//        System.out.println(new String(bytes));
+//        String path = "/FirstZnode";
+//        byte[] data = "first znode data".getBytes("utf8");
+//        List<ACL> acl = zooKeeper.getACL("/", new Stat());
 //        String success = zooKeeper.create(path, data, acl, CreateMode.PERSISTENT);
 //        System.out.println(success);
         List<String> list = zooKeeper.getChildren("/", false);
         list.stream().forEach(x -> System.out.println(x));
-        zooKeeper.setData("/FirstZnode", "new znode data".getBytes(), zooKeeper.exists("/FirstZnode", false).getVersion());
-        Stat stat =zooKeeper.exists("/FirstZnode", false);
-        System.out.println(stat);
+//        zooKeeper.setData("/FirstZnode", "new znode data".getBytes(), zooKeeper.exists("/FirstZnode", false).getVersion());
+//        Stat stat =zooKeeper.exists("/FirstZnode", false);
     }
 
 
